b05543eba52c27afd58712c3803a79ee34c13280
MohamedAtef308 <mimo3082002@gmail.com>
2024-08-11 17:46:54 +0300

    Navbar 90%

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import {LuArmchair} from "react-icons/lu"
+import {VscCode} from "react-icons/vsc"
+
+function Logo() {
+  return <Button size="icon">
+    <Link href="/">
+    <VscCode className="w-6 h-6"/>
+    </Link>
+  </Button>;
+}
+
+export default Logo;
